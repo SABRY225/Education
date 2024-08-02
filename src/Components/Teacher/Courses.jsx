381b25e7bcbd266a5d877b67ee4d5f5d4caa8878
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-08-02 03:27:17 +0300

    images

--- src/Components/Teacher/Courses.jsx
+++ src/Components/Teacher/Courses.jsx
@@ -7,7 +7,7 @@ const CourseCard = ({ imgSrc, courseName, onEdit, onDelete }) => {
     return (
         
         <div className="course-card">
-            <img src={imgSrc} alt={courseName} className="course-image" />
+            <img src={`http://localhost:5177/${imgSrc}`} alt={courseName} className="course-image" />
             <Link to="/DetailsCourse"><h3 className="course-name ">{courseName}</h3></Link>
         <div className="course-actions">
                 <Link to="/EditeCourse" onClick={onEdit} className="edit-button">تعديل</Link>
