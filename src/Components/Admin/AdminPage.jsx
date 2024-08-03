cd3bb50b7d5d03ab44f83904c5f09f51d3ee8b6d
Ahmed_Kamal <ahmed0a41468158@gmail.com>
2024-08-04 02:12:17 +0300

    complete admin dashboard

--- src/Components/Admin/AdminPage.jsx
+++ src/Components/Admin/AdminPage.jsx
@@ -2,10 +2,20 @@ import React, { useState } from 'react';
 import './AdminPage.css';
 import Statistics from './Statistics';
 import Enrolling from './Enrolling';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 function AdminPage() {
   const [activeComponent, setActiveComponent] = useState('statistics');
 
+  const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    // Logout function
+    const handleLogout = () => {
+        dispatch(signOutUser());
+        navigate('/signin'); // Redirect to the sign-in page after logout
+    };
   const handleComponentChange = (component) => {
     setActiveComponent(component);
   };
@@ -19,7 +29,7 @@ function AdminPage() {
         </div>
         <button onClick={() => handleComponentChange('statistics')}>الإحصائيات</button>
         <button onClick={() => handleComponentChange('enrolling')}>تسجيل طلاب</button>
-        <button className="logout-button">تسجيل خروج</button>
+        <button className="logout-button" onClick={handleLogout}>تسجيل خروج</button>
       </div>
       <div className="content">
         {activeComponent === 'statistics' && <Statistics />}
